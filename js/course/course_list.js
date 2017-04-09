0cb57d19d3ac0f92e32529c96b2f4284012e1bff
liling <601424295@qq.com>
2017-04-09 14:06:02 +0800

    获取课程列表

--- js/course/course_list.js
+++ js/course/course_list.js
@@ -1,3 +1,15 @@
-define([], function() {
-	
+define(['jquery', 'common', 'header', 'aside', 'nprogress', 'loading', 'template'], function($, undefined, undefined, undefined, nprogress, loading, template) {
+
+  template.helper('random', function(source, param) {
+    var params = param.split(', ');
+    return Math.ceil(Math.random() * (params[1] - params[0] + params[0]));
+  });
+
+  // 渲染课程列表
+  $.get('/v6/course', function(data) {
+    if(data.code == 200) {
+      $('.courses').append(template('course-list-tpl', data));
+    }
+  });
+  nprogress.done();
 });
